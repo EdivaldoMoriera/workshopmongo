1a15c47cd9f51d51a7ac38fb07cdaa7738c3d248
Moreira <valdosm.moreira@gmail.com>
2024-12-13 20:03:19 -0300

    conectando a base de dados

--- src/main/java/com/valdosm/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/valdosm/workshopmongo/WorkshopmongoApplication.java
@@ -1,13 +1,38 @@
 package com.valdosm.workshopmongo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+import com.valdosm.workshopmongo.domain.User;
+import com.valdosm.workshopmongo.repository.UserRepository;
+
 
 @SpringBootApplication
-public class WorkshopmongoApplication {
+@Configuration
+public class WorkshopmongoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository repo;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopmongoApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		repo.deleteAll();
+		User user =  new User(null, "Pedro", "pedro.moreira@gmail.com");
+		User user2 =  new User(null, "Carlos", "carlos.silva@gmail.com");
+		User user3 =  new User(null, "Maria", "maria.piedade@gmail.com");
+		User user4=  new User(null, "Joana", "joana.despointes@gmail.com");
+		repo.saveAll(Arrays.asList(user, user2, user3, user4));
+	}
+
 
 }
---
+++ src/main/java/com/valdosm/workshopmongo/resources/UserResource.java
@@ -0,0 +1,25 @@
+package com.valdosm.workshopmongo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.valdosm.workshopmongo.domain.User;
+import com.valdosm.workshopmongo.service.UserService;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
